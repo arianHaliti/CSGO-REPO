1fea381eb19f0bea06394d4f8cb86b89ee244768
Arian Haliti <arian93haliti@gmail.com>
2019-12-29 21:56:24 +0100

    adding online db and inv.json

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
 let env = process.env.NODE_ENV || "development";
 console.log(`Environment -> ${env}`);
 
-if (env === "development" || env === "server") {
-   let config = require("./config.json");
-   let envConfig = config[env];
+if (env === "development" || env === "server" || env === "development-online") {
+  let config = require("./config.json");
+  let envConfig = config[env];
 
-   Object.keys(envConfig).forEach(key => {
-      process.env[key] = envConfig[key];
-   });
+  Object.keys(envConfig).forEach(key => {
+    process.env[key] = envConfig[key];
+  });
 }
